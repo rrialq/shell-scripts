4fa0901443bf64bfbf4011c35fcf9ca919d50099
Ramón Rial <rrialq@gmail.com>
2018-01-11 22:05:34 +0100

    Update 0-bootstrap.sh

--- arch/0-bootstrap.sh
+++ arch/0-bootstrap.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+cd ${HOME}
+rm -Rf ${HOME}/shell-scripts-master
 loadkeys es
 # Customize the order of mirrorlist for Spain
 sed -i '1 i\Server = http://ftp.rediris.es/mirror/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist \
 && sed -i '1 i\Server = http://osl.ugr.es/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist \
 && sed -i '1 i\## Spain' /etc/pacman.d/mirrorlist \
 && pacman -Sy \
-&& pacman -Sy unzip \
+&& yes | pacman -S unzip \
 && wget https://github.com/rrialq/shell-scripts/archive/master.zip \
 && unzip master.zip \
-&& cd shell-scripts/arch \
+&& cd ${HOME}/shell-scripts-master/arch \
 && chmod +x *sh
