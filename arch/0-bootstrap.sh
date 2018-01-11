1cde7c40378027685bce748233235eabcb4b8dc0
Ramón Rial <rrialq@gmail.com>
2018-01-11 21:58:17 +0100

    Update 0-bootstrap.sh

--- arch/0-bootstrap.sh
+++ arch/0-bootstrap.sh
@@ -5,7 +5,8 @@ loadkeys es
 sed -i '1 i\Server = http://ftp.rediris.es/mirror/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist \
 && sed -i '1 i\Server = http://osl.ugr.es/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist \
 && sed -i '1 i\## Spain' /etc/pacman.d/mirrorlist \
-&& pacman -Syu && pacman -Sy unzip \
+&& pacman -Sy \
+&& pacman -Sy unzip \
 && wget https://github.com/rrialq/shell-scripts/archive/master.zip \
 && unzip master.zip \
 && cd shell-scripts/arch \
