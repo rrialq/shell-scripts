79bac39e15f1a088e4330b9303ae430cfeb1b808
Ramón Rial <rrialq@gmail.com>
2018-01-11 22:11:11 +0100

    Create configure-the-system.sh

---
+++ arch/configure-the-system.sh
@@ -0,0 +1,17 @@
+	Configure the system
+3.1	Fstab
+3.2	Chroot
+3.3	Time zone
+3.4	Locale
+3.5	Hostname
+3.6	Network configuration
+3.7	Initramfs
+3.8	Root password
+3.9	Boot loader
+
+. ./configure
+
+fstab() {
+    title '3.1 Generate an fstab file by UUID'
+    genfstab -U /mnt >> /mnt/etc/fstab
+}
