882e926189051dad8b259f730c93a36b4f5bb4eb
Ramón Rial <rrialq@gmail.com>
2018-01-11 22:43:31 +0100

    Update 3-configure-the-system.sh

--- arch/3-configure-the-system.sh
+++ arch/3-configure-the-system.sh
@@ -1,14 +1,4 @@
-	Configure the system
-3.1	Fstab
-3.2	Chroot
-3.3	Time zone
-3.4	Locale
-3.5	Hostname
-3.6	Network configuration
-3.7	Initramfs
-3.8	Root password
-3.9	Boot loader
-
+#!/bin/bash
 . ./configure
 
 fstab() {
