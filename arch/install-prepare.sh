ab6da466fd0e38a552f3b024063bfa22206644b4
Ramón Rial <rrialq@gmail.com>
2018-01-11 20:55:36 +0100

    Update install-prepare.sh

--- arch/install-prepare.sh
+++ arch/install-prepare.sh
@@ -6,6 +6,4 @@ sed -i '1 i\Server = http://ftp.rediris.es/mirror/archlinux/$repo/os/$arch' /etc
 sed -i '1 i\Server = http://osl.ugr.es/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
 sed -i '1 i\## Spain' /etc/pacman.d/mirrorlist
 
-pacman -Syu
-pacman -Sy git
-git clone https://github.com/rrialq/shell-scripts.git
+pacman -Syu && pacman -Sy git && git clone https://github.com/rrialq/shell-scripts.git && cd shell-scripts && echo 'Ok' || printf 'ERROR'
