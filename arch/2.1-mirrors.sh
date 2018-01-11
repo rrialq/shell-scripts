c823cad307a32e551bad05c14c0ea4100e66c46e
Ramón Rial <rrialq@gmail.com>
2018-01-11 20:45:12 +0100

    Delete 2.1-mirrors.sh

--- arch/2.1-mirrors.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Customize the order of mirrorlist for Spain
-sed -i '1 i\Server = http://ftp.rediris.es/mirror/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
-sed -i '1 i\Server = http://osl.ugr.es/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
-sed -i '1 i\## Spain' /etc/pacman.d/mirrorlist
-
-pacman -Sy
