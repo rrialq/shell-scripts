c50199e4f51c9a7bef3af19b936c228c0c0deaf6
Ramón Rial <rrialq@gmail.com>
2018-01-11 21:19:02 +0100

    Update 1-pre-installation.sh

--- arch/1-pre-installation.sh
+++ arch/1-pre-installation.sh
@@ -1,40 +1,47 @@
 #!/bin/bash
 
+. ./configuration
+
 setTheKeyboardLayout() {
     echo '1.1 Set the keyboard layout'
-    loadkeys es
+    loadkeys es > ${LOG_FILENAME} 2>&1
 }
 
 connectToTheInternet() {
     echo '1.3 Connect to the Internet'
-    ping archlinux.org
+    ping archlinux.org > ${LOG_FILENAME} 2>&1
 }
 
 updateTheSystemClock() {
     echo '1.4 Update the system clock'
-    timedatectl set-ntp true
+    timedatectl set-ntp true > ${LOG_FILENAME} 2>&1
 }
 
 partitionTheDisks() {
     echo '1.5 Partition the disks'
-    echo '    /Bios boot: 2M'
-    echo '    /boot: 1512M'
-    echo '    /: The rest of the disk'
+    echo      * Creating GPT Partition table'
     echo "label: gpt" | sfdisk /dev/sda
-    echo ',2M,21686148-6449-6E6F-744E-656564454649,*\n,1512M\n,,' | sfdisk /dev/sda
+    echo '    * Creating /Bios boot partition: 2M'
+    echo ',2M,21686148-6449-6E6F-744E-656564454649,*\n,1512M\n,,' | sfdisk /dev/sda > ${LOG_FILENAME} 2>&1
+    echo '    * Creating /boot partition: 1512M'
+    echo ',2M,21686148-6449-6E6F-744E-656564454649,*\n,1512M\n,,' | sfdisk /dev/sda > ${LOG_FILENAME} 2>&1
+    echo '    * Creating / partition: The rest of the disk'
+    echo ',2M,21686148-6449-6E6F-744E-656564454649,*\n,1512M\n,,' | sfdisk /dev/sda > ${LOG_FILENAME} 2>&1
 }
 
 formatThePartitions() {
     echo '1.6 Format the partitions'
-    mkfs.ext4 /dev/sda1
-    mkfs.ext4 /dev/sda2
+    echo '    * Formatting /dev/sda2 (/boot)'
+    mkfs.xfs /dev/sda2 > ${LOG_FILENAME} 2>&1
+    echo '    * Formatting /dev/sda3 (/)'
+    mkfs.xfs /dev/sda3 > ${LOG_FILENAME} 2>&1
 }
 
 mountThePartitions() {
     echo '1.7 Mount the partitions'
-    mount /dev/sda2 /mnt
+    mount /dev/sda3 /mnt
     mkdir /mnt/boot
-    mount /dev/sda1 /mnt/boot
+    mount /dev/sda2 /mnt/boot
 }
 
 echo '1. Pre-installation'
