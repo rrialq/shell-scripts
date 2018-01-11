69be9b7b3d315be675cca8df774f1ba9c598c196
Ramón Rial <rrialq@gmail.com>
2018-01-11 21:43:20 +0100

    Update 1-pre-installation.sh

--- arch/1-pre-installation.sh
+++ arch/1-pre-installation.sh
@@ -2,51 +2,58 @@
 
 . ./configuration
 
+
 exec 2> ${LOG_FILENAME}
+exec 3>&1
+
+title() {
+    echo ${1} >&3
+}
+
 
 setTheKeyboardLayout() {
-    echo '1.1 Set the keyboard layout'
+    title '1.1 Set the keyboard layout'
     loadkeys es
 }
 
 connectToTheInternet() {
-    echo '1.3 Connect to the Internet'
+    title '1.3 Connect to the Internet'
     ping -c1 archlinux.org
 }
 
 updateTheSystemClock() {
-    echo '1.4 Update the system clock'
+    title '1.4 Update the system clock'
     timedatectl set-ntp true
 }
 
 partitionTheDisks() {
-    echo '1.5 Partition the disks'
-    echo '    * Creating GPT Partition table'
+    title '1.5 Partition the disks'
+    title '    * Creating GPT Partition table'
     echo "label: gpt" | sfdisk /dev/sda 
-    echo '    * Creating /Bios boot partition: 2M'
+    title '    * Creating /Bios boot partition: 2M'
     echo ',2M,21686148-6449-6E6F-744E-656564454649,*' | sfdisk /dev/sda
-    echo '    * Creating /boot partition: 1512M'
+    title '    * Creating /boot partition: 1512M'
     echo ',1512M' | sfdisk -a /dev/sda
-    echo '    * Creating / partition: The rest of the disk'
+    title '    * Creating / partition: The rest of the disk'
     echo ',,' | sfdisk -a /dev/sda
 }
 
 formatThePartitions() {
-    echo '1.6 Format the partitions'
-    echo '    * Formatting /dev/sda2'
+    title '1.6 Format the partitions'
+    title '    * Formatting /dev/sda2'
     mkfs.xfs -f /dev/sda2
-    echo '    * Formatting /dev/sda3'
+    title '    * Formatting /dev/sda3'
     mkfs.xfs -f /dev/sda3
 }
 
 mountTheFileSystems() {
-    echo '1.7 Mount the partitions'
+    title '1.7 Mount the partitions'
     mount /dev/sda3 /mnt 
     mkdir /mnt/boot
     mount /dev/sda2 /mnt/boot
 }
 
-echo '1. Pre-installation'
+title '1. Pre-installation'
 setTheKeyboardLayout \
 && connectToTheInternet \
 && updateTheSystemClock \
