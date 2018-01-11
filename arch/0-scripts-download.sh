9e7efc46efed89897ca244d94bc2a88af00560ef
Ramón Rial <rrialq@gmail.com>
2018-01-11 21:52:13 +0100

    Update 0-scripts-download.sh

--- arch/0-scripts-download.sh
+++ arch/0-scripts-download.sh
@@ -2,8 +2,11 @@
 
 loadkeys es
 # Customize the order of mirrorlist for Spain
-sed -i '1 i\Server = http://ftp.rediris.es/mirror/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
-sed -i '1 i\Server = http://osl.ugr.es/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
-sed -i '1 i\## Spain' /etc/pacman.d/mirrorlist
-
-pacman -Syu && pacman -Sy git && git clone https://github.com/rrialq/shell-scripts.git && cd shell-scripts && echo 'Ok' || printf 'ERROR'
+sed -i '1 i\Server = http://ftp.rediris.es/mirror/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist \
+&& sed -i '1 i\Server = http://osl.ugr.es/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist \
+&& sed -i '1 i\## Spain' /etc/pacman.d/mirrorlist \
+&& pacman -Syu && pacman -Sy unzip \
+&& wget https://github.com/rrialq/shell-scripts/archive/master.zip \
+&& unzip master.zip \
+&& cd shell-scripts/arch \
+&& chmod +x *sh
