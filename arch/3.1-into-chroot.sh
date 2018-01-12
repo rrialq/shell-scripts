88fb1dbaa2b9a3e561d198474ba00fdf8c979ea6
Ramón Rial <rrialq@gmail.com>
2018-01-12 19:30:57 +0100

    Update 3.1-into-chroot.sh

--- arch/3.1-into-chroot.sh
+++ arch/3.1-into-chroot.sh
@@ -7,11 +7,17 @@ title '3.3 Time zone'
 ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime \
 && hwclock --systohc \
 && title '3.4 Locale' \
+&& title '    * Setting es_ES.UTF-8' \
 && sed -i 's/#es_ES.UTF-8 UTF-8/es_ES.UTF-8 UTF-8/' /etc/locale.gen \
+&& title '    * Setting en_US.UTF-8' \
 && sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen \
+&& title '    * Running locale-gen' \
 && locale-gen \
+&& title '    * Setting LANG' \
 && echo "LANG=es_ES.UTF-8" > /etc/locale.conf \
+&& title '    * Setting KEYMAP' \
 && echo "KEYMAP=es" > /etc/vconsole.conf \
+&& title '    * Creating 10-keyboard.conf' \
 && echo 'Section "InputClass"
     Identifier "system-keyboard"
 	  MatchIsKeyboard "on"
