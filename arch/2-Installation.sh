8b7aad23028562522351812dbad1877730b06cfb
Ramón Rial <rrialq@gmail.com>
2018-01-12 20:16:33 +0100

    Update 2-Installation.sh

--- arch/2-Installation.sh
+++ arch/2-Installation.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
+. ./configuration
+
 # Not necessary, since contained in 0-scripts-download.sh
 selectTheMirrors() {
-    printf '2.1 Select the mirrors '
     # Customize the order of mirrorlist for Spain
-    sed -i '1 i\Server = http://ftp.rediris.es/mirror/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist \
+    title '2.1 Select the mirrors ' \
     && sed -i '1 i\Server = http://osl.ugr.es/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist \
+    && sed -i '1 i\Server = http://ftp.rediris.es/mirror/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist \
     && sed -i '1 i\## Spain' /etc/pacman.d/mirrorlist \
     && pacman -Sy
 }
 
 installTheBasePackages() {
-    printf '2.2 Install the base packages '
-    pacstrap /mnt base grub intel-ucode sudo
+    printf '2.2 Install the base packages ' \
+    && pacstrap /mnt base grub intel-ucode sudo
 }
 
 installTheBasePackages
