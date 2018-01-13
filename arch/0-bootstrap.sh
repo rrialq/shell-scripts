1e3f107e62abe62248e9e241dd141088f60e3d91
Ramón Rial <rrialq@gmail.com>
2018-01-13 12:25:54 +0100

    Update 0-bootstrap.sh

--- arch/0-bootstrap.sh
+++ arch/0-bootstrap.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+selectTheMirrors() {
+    printf '2.1 Select the mirrors '
+    # Customize the order of mirrorlist for Spain
+    sed -i '1 i\Server = http://ftp.rediris.es/mirror/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist \
+    && sed -i '1 i\Server = http://osl.ugr.es/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist \
+    && sed -i '1 i\## Spain' /etc/pacman.d/mirrorlist \
+    && pacman -Sy
+}
+
 cd ${HOME}
 rm -Rf ${HOME}/shell-scripts-master
-loadkeys es
-# Customize the order of mirrorlist for Spain
-sed -i '1 i\Server = http://ftp.rediris.es/mirror/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist \
-&& sed -i '1 i\Server = http://osl.ugr.es/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist \
-&& sed -i '1 i\## Spain' /etc/pacman.d/mirrorlist \
+export LC_ALL=es_ES.UTF-8
+loadkeys es \
+&& selectTheMirrors \
 && pacman -Sy \
 && yes | pacman -S unzip \
 && wget https://github.com/rrialq/shell-scripts/archive/master.zip \
 && unzip master.zip \
-&& chmod +x ${HOME}/shell-scripts-master/arch/*sh
+&& chmod +x ${HOME}/shell-scripts-master/arch/*sh \
+&& title '\n${0} OK\n' 
