c1533504e313564c562ec98a66f8f120bd5514ff
Ramón Rial <rrialq@gmail.com>
2018-01-24 15:36:48 +0100

    Update virtualbox-guest-additions-install.sh

--- arch/virtualbox-guest-additions-install.sh
+++ arch/virtualbox-guest-additions-install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-
-printf '         * Virtualbox Guest Additions installing...'
-sudo pacman -Sy linux-lts linux-lts-headers virtualbox-guest-modules-arch virtualbox-guest-utils && printf '\r[OK   ]\n' || printf '\r[ERROR]\n'
+CONF_FILENAME='/etc/modules/load.d/virtualbox.conf'
+title 'Virtualbox Guest Additions installing...' \
+&& sudo pacman -S linux-lts linux-lts-headers virtualbox-guest-modules-arch virtualbox-guest-utils \
+&& title '* Modprobe' \
+&& sudo modprobe -a vboxguest vboxsf vboxvideo \
+&& title '* Creating virtualbox.conf' \
+&& sudo echo 'vboxguest\nvboxsf\nvboxvideo' > ${CONF_FILENAME} \
+&& title '* Enabling modules' \
+&& VBoxClient-all \
+&& printf '\r[OK   ]\n' || printf '\r[ERROR]\n'
