2b906d5b4971714dec6e7e60fbf475862e0e3076
Ramón Rial <rrialq@gmail.com>
2018-01-24 15:47:21 +0100

    Update pending-after-X.sh

--- arch/pending-after-X.sh
+++ arch/pending-after-X.sh
@@ -1,9 +1,9 @@
 && title '    * Creating 10-keyboard.conf' \
 && echo 'Section "InputClass"
     Identifier "system-keyboard"
-	  MatchIsKeyboard "on"
-	  Option "XkbLayout"  "es,us"
+    MatchIsKeyboard "on"
+    Option "XkbLayout"  "es,us"
     Option "XkbModel"   "pc104"
     Option "XkbVariant" "deadtilde,dvorak"
     Option "XkbOptions" "grp:alt_shift_toggle"
-EndSection' >> /etc/X11/xorg.conf.d/10-keyboard.conf \
+EndSection' | sudo tee --append /etc/X11/xorg.conf.d/10-keyboard.conf > /dev/null
